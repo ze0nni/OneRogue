1dd1fb66c43c1dd01782c6f5cc367903831bb141
e.blagodarev <ze0nni@gmail.com>
2020-04-09 12:49:25 +0300

    Init unity project

---
+++ Assets/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 centerOfMass = new Vector3(0, -2, 0);
+    public float centerOfMassMovementOffset = 1;
+
+    public float forwardMoveForce = 10f;
+
+    Rigidbody rigidbody;
+    void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var angle = rigidbody.transform.rotation.ToEulerAngles().y * Mathf.Deg2Rad;
+        var lookVector = new Vector2(Mathf.Cos(angle), -Mathf.Sin(angle));
+
+        var movDirection = new Vector2();
+        if (Input.GetKey(KeyCode.D)) { movDirection.y += lookVector.x; movDirection.x += lookVector.y; }
+        if (Input.GetKey(KeyCode.A)) { movDirection.y -= lookVector.x; movDirection.x -= lookVector.y; }
+        if (Input.GetKey(KeyCode.W)) { movDirection.x += lookVector.x; movDirection.y += lookVector.y; }
+        if (Input.GetKey(KeyCode.S)) { movDirection.x -= lookVector.x; movDirection.y -= lookVector.y; }
+        movDirection.Normalize();
+
+        rigidbody.AddForce(
+            movDirection.y * forwardMoveForce,
+            0,
+            movDirection.x * forwardMoveForce
+        );
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rigidbody.AddForce(0, 300, 0);
+        }
+
+        rigidbody.centerOfMass = new Vector3(
+            centerOfMass.x + movDirection.y * centerOfMassMovementOffset,
+            centerOfMass.y,
+            centerOfMass.z + movDirection.x * centerOfMassMovementOffset
+        );
+    }
+
+    void OnDrawGizmos() {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawIcon(transform.position + transform.rotation * rigidbody.centerOfMass, "food_apple.png", true);
+    }
+}
