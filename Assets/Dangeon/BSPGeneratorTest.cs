05f4f27139595d4414711489d1a08332cd1cdc7e
e.blagodarev <ze0nni@gmail.com>
2020-04-14 00:19:44 +0300

    Implements BSPGenerator

--- Assets/Dangeon/BSPGeneratorTest.cs
+++ Assets/Dangeon/BSPGeneratorTest.cs
@@ -12,34 +12,39 @@
         public RectTransform DangeonDisplay;
 
         private System.Random random = new System.Random();
-        private BSPGenerator generator = new BSPGenerator(5, 30, 1, 0.5f);
+        private BSPGenerator generator = new BSPGenerator(5, 20, 0.5f);
 
         void Start()
         {
 
         }
 
-        public void Generate() {
+        public void Generate()
+        {
             ClearDisplay();
-            foreach (var rect in generator.Generate((int)Width.value, (int)Height.value, random)) {
+            foreach (var rect in generator.Generate((int)Width.value, (int)Height.value, random))
+            {
+                Debug.Log(rect.ToString());
                 InsertRoom(rect);
             }
         }
 
-        private void ClearDisplay()  {
+        private void ClearDisplay()
+        {
             foreach (Transform childTransform in DangeonDisplay.transform) Destroy(childTransform.gameObject);
         }
 
-        private void InsertRoom(RectInt rect) {
+        private void InsertRoom(RectInt rect)
+        {
             var room = new GameObject("Room", typeof(RectTransform), typeof(Image));
             room.transform.SetParent(DangeonDisplay.transform);
-            room.GetComponent<Image>().color = Color.black;
+            room.GetComponent<Image>().color = new Color(Random.value * 0.5f + 0.5f, Random.value * 0.5f + 0.5f, Random.value * 0.5f + 0.5f);
 
             var transform = room.GetComponent<RectTransform>();
             transform.pivot = new Vector2(0, 1);
-            transform.localPosition = new Vector3(rect.x * 10, -rect.y * 10, 0);
-            transform.sizeDelta = rect.size * 10;
+            transform.localPosition = new Vector3(rect.x * 10 + 1, -rect.y * 10 + 1, 0);
+            transform.sizeDelta = rect.size * 10 - new Vector2Int(2, 2);
         }
     }
-
 }
+
