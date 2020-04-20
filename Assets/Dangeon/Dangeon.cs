fe9894db7d1cb782ab7b1990e2faee731fe6b11a
e.blagodarev <ze0nni@gmail.com>
2020-04-20 11:49:59 +0300

    Remove legasy rooms and generator

--- Assets/Dangeon/Dangeon.cs
+++ Assets/Dangeon/Dangeon.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dangeon
 {
-
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
@@ -9,9 +9,9 @@
     public class Dangeon : MonoBehaviour
     {
         public Player player;
-        public DangeonGeneratorData DangeonGeneratorData;
         public GameObject RoomPrefab;
 
+        private GameObject[] rooms;
         private GameObject[] monsters;
         private GameObject[] inventoryItems;
 
@@ -25,66 +25,24 @@
         IEnumerator PrepareDangeon() {
             Debug.Log("Start...");
 
-            yield return new WaitForEndOfFrame();
-            this.monsters = Resources.LoadAll<GameObject>("Monsters");
-            Debug.Log(string.Format("{0} Monsters loaded", monsters.Length));
+            var tasks = new (string, Action<GameObject[]>)[]
+            {
+                ("Rooms", result => this.rooms = result),
+                ("Monsters", result => this.monsters = result),
+                ("Inventory", result => this.inventoryItems = result)
+            };
 
-            yield return new WaitForEndOfFrame();
-            this.inventoryItems = Resources.LoadAll<GameObject>("Inventory");
-            Debug.Log(string.Format("{0} Inventory items loaded", inventoryItems.Length));
+            foreach (var t in tasks) {
+                yield return new WaitForEndOfFrame();
+                var result = Resources.LoadAll<GameObject>(t.Item1);
+                t.Item2.Invoke(result);
+                Debug.Log(string.Format("Loaded: {0} {1}", result.Length, t.Item1));
+            }
 
-            yield return new WaitForEndOfFrame();
-            var random = new System.Random();
-            var generator = new DangeonGenerator(DangeonGeneratorData);
-            var dangeon = generator.Generate(24, 24, 1, random);
+            
             Debug.Log("Dangeon generated");
 
-            yield return new WaitForEndOfFrame();
-            foreach (var r in dangeon.Rooms)
-            {
-                InsertRoom(r);
-            }
-
-            var start = dangeon.Rooms[0];
-            player.transform.position = new Vector3(
-                start.center.x * 5,
-                5,
-                start.center.y * 5
-            );
-
             player.gameObject.SetActive(true);
-        }
-
-        private void InsertRoom(RectInt room) {
-            Instantiate(RoomPrefab, this.transform)
-                .GetComponent<RoomLegasy>()
-                .Create(DangeonGeneratorData, room);
-
-            var monstersCount = Random.Range(3, 7);
-            for (var i = 0; i < monstersCount; i++)
-            {
-                var monsterData = monsters[Random.Range(0, monsters.Length)];
-
-                var monster = Instantiate(monsterData, this.transform);
-                monster.transform.localPosition = new Vector3(
-                    (room.x + Random.Range(0, room.width)) * 5 + 2.5f,
-                    1,
-                    (room.y + Random.Range(0, room.height)) * 5 + 2.5f
-                );
-            }
-
-            var invCount = Random.Range(3, 7);
-            for (var i = 0; i < invCount; i++)
-            {
-                var invData = inventoryItems[Random.Range(0, inventoryItems.Length)];
-
-                var inv = Instantiate(invData, this.transform);
-                inv.transform.localPosition = new Vector3(
-                    (room.x + Random.Range(0, room.width)) * 5 + 2.5f,
-                    1,
-                    (room.y + Random.Range(0, room.height)) * 5 + 2.5f
-                );
-            }
         }
     }
 
--- Assets/Dangeon/DangeonGenerator.cs
+++ Assets/Dangeon/DangeonGenerator.cs
@@ -1,109 +1,12 @@
 ﻿namespace Dangeon
 {
-    using global::Dangeon.Generator;
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
 
     public class DangeonGenerator
     {
-        private DangeonGeneratorData data;
-        private List<CellMap> cells = new List<CellMap>();
-        public IReadOnlyList<CellMap> Cells { get => cells; }
-
-        public DangeonGenerator(DangeonGeneratorData data) {
-            this.data = data;
-
-            foreach (var d in data.Tiles) {
-                var waypoints = new List<WaypointArea>();
-                FindWaypoints(d.tilePrefab, waypoints);
-                var map = ToCellMap(d, waypoints);
-                cells.Add(map);
-                for (var i = 0; i < 3; i++) {
-                    map = TurnCellMap(map);
-                    cells.Add(map);
-                }
-            }
-        }
-
-        public struct DangeonData {
-            public List<RectInt> Rooms;
-            public List<(RectInt, RectInt)> Links;
-        }
-
-        public DangeonGenerator.DangeonData Generate(int width, int height, int levels, System.Random random) {
-            var bsp = new BSPGenerator(3, 6, 0.5f);
-            var triangulator = new RoomsTriangulator<RectInt>(r => r);
-
-            var rooms = bsp.Generate(width, height, random);
-            var links = triangulator.Generate(rooms);
-
-            return new DangeonData()
-            {
-                Rooms = rooms,
-                Links = links
-            };
-        }
-
-        public enum CellType { None, Way, Exit }
-
-        public struct CellMap {
-            public DangeonGeneratorData.TileData Tile;
-            public int Rotation;
-            public CellType[,,] map;
-
-        }
-
-        static private void FindWaypoints(GameObject go, List<WaypointArea> waypoints) {
-            var wp = go.GetComponent<WaypointArea>();
-            if (null != wp) {
-                waypoints.Add(wp);
-            }
-            for (var i = 0; i < go.transform.childCount; i++) {
-                FindWaypoints(go.transform.GetChild(i).gameObject, waypoints);
-            }
-        }
-
-        static CellMap ToCellMap(
-            DangeonGeneratorData.TileData tile,
-            List<WaypointArea> areas
-        ) {
-            var map = new CellType[5, 6, 5];
-
-            foreach (var a in areas) {
-                foreach(var p in a.Points) {
-                    map[p.position.x, p.position.y, p.position.z] = p.IsExitPoint ? CellType.Exit : CellType.Way;
-                }
-            }
-
-            return new CellMap()
-            {
-                Tile = tile,
-                Rotation = 0,
-                map = map
-            };
-        }
-
-        static CellMap TurnCellMap(CellMap input) {
-            var map = new CellType[5, 6, 5];
-
-            for (var x = 0; x < 5; x ++) {
-                for (var y = 0; y < 6; y++)
-                {
-                    for (var z = 0; z < 5; z++)
-                    {
-                        map[x, y, z] = input.map[z, y, 4 -  x];
-                    }
-                }
-            }
-
-            return new CellMap()
-            {
-                Tile = input.Tile,
-                Rotation = input.Rotation + 1,
-                map = map
-            };
-        }
+        
     }
 
 }
